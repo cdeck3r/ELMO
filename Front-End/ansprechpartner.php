5f4b5a4fec502bedda31bb952bfb2684a8a544cc
Patrick-Ardelean <44040655+Patrick-Ardelean@users.noreply.github.com>
2019-01-08 02:03:22 +0100

    Update ansprechpartner.php

--- Front-End/ansprechpartner.php
+++ Front-End/ansprechpartner.php
@@ -41,13 +41,6 @@ $row1 = mysqli_fetch_array($result1);
     <title>ELMO CLOUD | Ansprechpartner</title>
     <!-- Bootstrap Core CSS -->
     <link href="../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    
-    <!-- chartist CSS -->
-    <link href="../assets/plugins/chartist-js/dist/chartist.min.css" rel="stylesheet">
-    <link href="../assets/plugins/chartist-js/dist/chartist-init.css" rel="stylesheet">
-    <link href="../assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.css" rel="stylesheet">
-    <!--This page css - Morris CSS -->
-    <link href="../assets/plugins/c3-master/c3.min.css" rel="stylesheet">
     <!-- Custom CSS -->
     <link href="css/style.css" rel="stylesheet">
     <!-- You can change the theme colors from here -->
@@ -57,10 +50,6 @@ $row1 = mysqli_fetch_array($result1);
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-
-
-
-
 <![endif]-->
 </head>
 
